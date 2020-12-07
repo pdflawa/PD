bfee84342d9cffcc8bcecec6af9f28e18bec2f38
pdflawa <sickshit97@mail.ru>
2020-12-07 15:57:14 +0400

    up

---
+++ Assets/Scripts/first.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class first : MonoBehaviour
+{
+    public bool up;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(!up)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime);
+            if(timer >= 0.5f)
+            {
+            up = true;
+            timer = 0;
+            }
+        }  
+
+        else if(up)
+        {
+            transform.Translate(Vector3.up * -1 * Time.deltaTime);
+            if(timer >= 0.5f)
+            {
+            up = false;
+            timer = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/second.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class second : MonoBehaviour
+{
+    public bool up;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(!up)
+        {
+            transform.Rotate (Vector3.up * -500 * Time.deltaTime);
+            if(timer >= 1f)
+            {
+            up = true;
+            timer = 0;
+            }
+        }  
+
+        else if(up)
+        {
+            transform.Rotate (Vector3.up * 500 * Time.deltaTime);
+            if(timer >= 1f)
+            {
+            up = false;
+            timer = 0;
+            }
+        }
+    }
+}
